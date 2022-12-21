5ee5c27df1f97d399590bb3d06851177b6f1b96e
nirazul <rouven@adorable-squid.ch>
2022-12-21 17:02:44 +0100

    Implement basic test fixture for yarn and experiment with runner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,10 @@
 <template>
   <main>
     <h1>Home</h1>
+    <RouterLink to="/story">
+      to story
+    </RouterLink>
   </main>
 </template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/StoryView.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+  // @ts-ignore
+  import YarnBound from 'yarn-bound/src'
+  import dialogue from '../assets/yarn/test.yarn?raw'
+  import { onMounted } from 'vue'
+
+  interface YarnBoundOptions {
+    dialogue: string
+    startAt?: string
+    functions?: Record<string, (...args: Array<unknown>) => unknown>
+    variableStorage?: unknown
+    handleCommand?: (...args: Array<unknown>) => unknown
+    combineTextAndOptionsResults?: boolean
+    locale?: string
+  }
+
+  onMounted(() => {
+    const runner = new YarnBound({
+      dialogue,
+    } as YarnBoundOptions)
+
+    console.log(runner.currentResult)
+  })
+</script>
+
+<template>
+  <div class="about">
+    <h1>Story</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
