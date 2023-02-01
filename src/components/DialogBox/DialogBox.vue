c4b8ec08cb6663a7d833c13b3a919fff73ebe74a
nirazul <rouven@adorable-squid.ch>
2023-02-01 15:31:42 +0100

    Add text entry animation

--- src/components/DialogBox/DialogBox.vue
+++ src/components/DialogBox/DialogBox.vue
@@ -2,50 +2,68 @@
   // @ts-ignore
   import YarnBound from 'yarn-bound/src'
   import { cloneDeep } from 'lodash-es'
-  import { computed, nextTick, ref, watch } from 'vue'
+  import { computed, ref, watch } from 'vue'
   import { DialogResultType } from '@/models/DialogResult/DialogResult'
   import useDialogResult from '@/composables/DialogResult/DialogResult'
+  import UiAccordion from '@/components/UiAccordion/UiAccordion.vue'
 
   export interface Props {
     facets?: Array<string>
     runner: YarnBound
   }
 
+  interface ResultHistoryEntry {
+    timestamp: number
+    isOpen: boolean
+    result: YarnBound.Result
+  }
+
   const props = defineProps<Props>()
   const { getResultType, getResultComponent } = useDialogResult()
 
   const scrollEl = ref<HTMLDivElement | null>(null)
-  const resultHistory = ref<Array<YarnBound.Result>>([])
+  const resultHistory = ref<Array<ResultHistoryEntry>>([])
   const currentResult = computed(() => props.runner.currentResult)
   const currentResultType = computed<DialogResultType>(() => {
     return getResultType(currentResult.value)
-  })
-  const currentResultComponent = computed<string>(() => {
-    return getResultComponent(currentResult.value)
   })
   const isAdvanceDisabled = computed<boolean>(() => {
     return [DialogResultType.Options, DialogResultType.End].includes(currentResultType.value)
   })
 
   const advance = () => {
-    resultHistory.value.push(cloneDeep(currentResult.value))
     props.runner.advance()
-
-    nextTick(scrollToBottom)
+    // nextTick(scrollToBottom)
   }
+
+  // const scrollToBottom = () => {
+  //   scrollEl.value?.scroll({ top: scrollEl.value?.scrollHeight, behavior: 'smooth' })
+  // }
 
   const onOptionChosen = (optionIdx: number) => {
     props.runner.advance(optionIdx)
   }
 
-  const scrollToBottom = () => {
-    scrollEl.value?.scroll({ top: scrollEl.value?.scrollHeight, behavior: 'smooth' })
+  const onAfterEnter = (entry: ResultHistoryEntry) => {
+    if (entry) {
+      entry.isOpen = true
+    }
   }
 
   watch(
     currentResult,
-    () => {
-      console.log(currentResult.value)
+    (value, prevValue) => {
+      resultHistory.value
+        .filter(({ result }) => getResultType(result) === DialogResultType.Options)
+        .forEach((result) => {
+          result.isOpen = false
+        })
+
+      resultHistory.value.push({
+        timestamp: Date.now(),
+        isOpen: false,
+        result: cloneDeep(currentResult.value),
+      })
     },
     { immediate: true },
   )
@@ -58,16 +76,35 @@
         <div class="c-dialog-box__wrap">
           <div ref="scrollEl" class="c-dialog-box__scroller">
             <ul class="c-dialog-box__history-entries u-reset">
-              <li v-for="result in resultHistory" class="c-dialog-box__history-entry">
-                <component :is="getResultComponent(result)" v-bind="result" />
-              </li>
-              <li class="c-dialog-box__current-entry">
-                <component :is="currentResultComponent" v-bind="currentResult" @choose="onOptionChosen($event)" />
+              <li v-for="entry in resultHistory" class="c-dialog-box__history-entry">
+                <Transition appear :name="'trs-simple-fade'" @after-enter="onAfterEnter(entry)">
+                  <UiAccordion
+                    :data-open="entry.isOpen"
+                    :key="entry.timestamp"
+                    :facets="[]"
+                    :open="entry.isOpen"
+                    :passive="true"
+                  >
+                    <template #body>
+                      <component
+                        :is="getResultComponent(entry.result)"
+                        v-bind="entry.result"
+                        @choose="onOptionChosen($event)"
+                      />
+                    </template>
+                  </UiAccordion>
+                </Transition>
               </li>
             </ul>
           </div>
           <div class="c-dialog-box__actions">
-            <button :disabled="isAdvanceDisabled" class="c-dialog-box__advance btn btn--primary btn--small" @click="advance">weiter</button>
+            <button
+              :disabled="isAdvanceDisabled"
+              class="c-dialog-box__advance btn btn--primary btn--small"
+              @click="advance"
+            >
+              weiter
+            </button>
           </div>
         </div>
       </div>
@@ -117,6 +154,10 @@
     bottom: 0;
     left: 0;
     width: 100%;
+  }
+
+  .c-dialog-box__history-entry {
+    display: block;
   }
 
   .c-dialog-box__actions {
